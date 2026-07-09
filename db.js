e9ce94067d91f2448cb69f0ff83c3d3a5b22b11e
philip <ichinegbo@gmail.com>
2026-07-10 00:42:14 +0100

    Resolve conflict - keep local backend

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -23,14 +22,3 @@ CREATE TABLE IF NOT EXISTS users (
 pool.query(createTableQuery)
   .then(() => console.log("Users table ready"))
   .catch(err => console.log("Table creation error:", err));
-=======
- const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = pool;
->>>>>>> f935380ef35bba2f0d262ae2c80172b75bc309fa
